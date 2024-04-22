980bf84e919f8dbc01912f02fc38c63c8ecdf8a0
Philip Japikse <skimedic@gmail.com>
2024-04-22 16:12:57 -0400

    formatting

--- Code/CompletedLabs/B_API/Lab_API02/AutoLot.Api/Program.cs
+++ Code/CompletedLabs/B_API/Lab_API02/AutoLot.Api/Program.cs
@@ -56,9 +56,6 @@ builder.Services.AddCors(options =>
     });
 });
 
-
-
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
